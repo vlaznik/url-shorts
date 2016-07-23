f13862e2d98e1eb887e7db149d16e7a7d28798f6
Victor Laznik <victor.laznik@gmail.com>
2016-07-23 14:12:43 -0400

    4th try to deploy

--- config/puma.rb
+++ config/puma.rb
@@ -13,8 +13,9 @@ port        ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
-web: bundle exec puma -t 5:5 -p ${PORT:-3000} -e ${RACK_ENV:-development}
+environment ENV.fetch("RAILS_ENV") { "development" }
 
+web: bundle exec puma -t 5:5 -p ${PORT:-3000} -e ${RACK_ENV:-development
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
 # the concurrency of the application would be max `threads` * `workers`.

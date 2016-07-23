6e2396314d064502053867ca5524b8627f33b44b
Victor Laznik <victor.laznik@gmail.com>
2016-07-23 14:43:10 -0400

    updated controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,9 @@ class WelcomeController < ApplicationController
   def index
   end
   def create
-    @url = Url.find(1)
+    @url = Url.new(id: '1')
+    @url.save
+    @url.find(1)
     redirect_to "http://www.google.com"
   end
 end

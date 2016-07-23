ddedc093fc9f6b359227faa3ece67bbef3033ec5
Victor Laznik <victor.laznik@gmail.com>
2016-07-23 14:10:58 -0400

    third attempt to deploy

--- config/puma.rb
+++ config/puma.rb
@@ -13,7 +13,7 @@ port        ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
-web: bundle exec puma -C config/puma.rb
+web: bundle exec puma -t 5:5 -p ${PORT:-3000} -e ${RACK_ENV:-development}
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
